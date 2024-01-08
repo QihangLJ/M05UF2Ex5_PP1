53d989ca5dfc3cdb412d0ce3447a8d777056d878
argo <argo@MAQUETA>
2024-01-08 13:25:18 +0100

    Add project files.

---
+++ M03UF2Ex2_Examen/Class.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M03UF2Ex2_Examen
+{
+    internal class ExMenu
+    {
+        public static bool NaturalNumber(int num)
+        {
+            return num >= 0;
+        }
+
+        public static bool OddNumber(int num)
+        {
+            return num % 2 != 0;
+        }
+        public static int CalculatePower(int baseNum, int expNum)
+        {
+            int res = 1;
+
+            for (int i = 0; i < expNum; i++)
+            {
+                res *= baseNum;
+            }
+
+            return res;
+        }
+
+        public static int RandomNumber(int min, int max)
+        {
+            Random rand = new Random();
+            return rand.Next(min, max);
+        }
+
+        public static int CountCharacter(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < charArray.Length;i++)
+            {
+                if (charArray[i] != ' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ M03UF2Ex2_Examen/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using M03UF2Ex2_Examen;
+namespace M03UF2EX1
+{
+    class MyProgram
+    {
+        static void Main()
+        {
+            const string MsgMenu = "MENU: \na. Validar si un nombre es senar \nb. Calcular la potència d'un nombre \nc. Retornar un valor aleatori \nd. Comptar el nombre de vocals o consonants en un text \ne. Sortir";
+            const string AskValue = "Enter a number:";
+            const string AskBaseAndExpo = "Enter a base and a Exponent:";
+            const string AskMinAndMax = "Enter the min and the max for the random rang:";
+            const string AskText = "Enter a text:";
+            const string MsgResult = "RESULT: ";
+
+            char selection;
+            bool exit = false, boolResult;
+            int input, intResult,bas, exp, min, max;
+            string inputText;
+
+            while (!exit)
+            {
+                Console.WriteLine(MsgMenu);
+
+                selection = Convert.ToChar(Console.ReadLine());
+
+                switch (selection)
+                {
+                    case 'a':
+                    case 'A':
+                        Console.WriteLine(AskValue);
+                        input = Convert.ToInt32(Console.ReadLine());
+                        boolResult = ExMenu.OddNumber(input);
+                        Console.WriteLine(MsgResult + boolResult);
+                        break;
+                    case 'b':
+                    case 'B':
+                        Console.WriteLine(AskBaseAndExpo);
+                        bas = Convert.ToInt32(Console.ReadLine());
+                        exp = Convert.ToInt32(Console.ReadLine());
+                        intResult = ExMenu.CalculatePower(bas, exp);
+                        Console.WriteLine(MsgResult + intResult);
+
+                        break;
+                    case 'c':
+                    case 'C':
+                        Console.WriteLine(AskMinAndMax);
+                        min = Convert.ToInt32(Console.ReadLine());
+                        max = Convert.ToInt32(Console.ReadLine());
+                        intResult = ExMenu.RandomNumber(min, max);
+                        Console.WriteLine(MsgResult + intResult);
+
+                        break;
+                    case 'd':
+                    case 'D':
+                        Console.WriteLine(AskText);
+                        inputText = Console.ReadLine();
+                        intResult = ExMenu.CountCharacter(inputText);
+                        Console.WriteLine(MsgResult + intResult);
+
+                        break;
+                    case 'e':
+                    case 'E':
+                        exit = true;
+                        break;
+                }
+            }
+        }
+    }
+}
+
